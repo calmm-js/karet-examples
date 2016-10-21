86e83ca2bf8564ae1f55461e81f5a5b28ddf594a
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-10-21 21:36:19 +0300

    Use `.into` on output variables.

--- src/converter.js
+++ src/converter.js
@@ -1,4 +1,5 @@
 import Atom   from "kefir.atom"
+import K      from "karet.util"
 import React  from "karet"
 
 import {NumberInput} from "./restricted-input"
@@ -8,8 +9,8 @@ export default ({celcius = Atom(0), fahrenheit = Atom()}) =>
     <NumberInput type="text" value={celcius}/>°C
     <br/>
     <NumberInput type="text" value={fahrenheit}/>°F
-    {celcius.do(c => fahrenheit.set(c * 9/5 + 32))}
-    {fahrenheit.do(f => celcius.set(5/9 * (f - 32)))}
+    {K(celcius, c => c * 9/5 + 32).into(fahrenheit)}
+    {K(fahrenheit, f => 5/9 * (f - 32)).into(celcius)}
   </p>
 
 // NOTE: The above pattern of using a cycle of updates (c -> f -> c) is not
--- src/monkey.js
+++ src/monkey.js
@@ -1,13 +1,15 @@
 import * as Kefir from "kefir"
 import K          from "karet.util"
 
-Kefir.Observable.prototype.mapK = function (mapping) {
-  return K(this, mapping)
+const o = Kefir.Observable.prototype
+
+o.startWith = function (v) {
+  return Kefir.constant(v).concat(this)
 }
 
-Kefir.Observable.prototype.do = function (action) {
-  return this.mapK(v => {
-    action(v)
+o.into = function (settable) {
+  return K(this, v => {
+    settable.set(v)
     return null
-  })
+  }).startWith(null)
 }
